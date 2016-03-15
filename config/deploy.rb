bc2dd2cd160736a7de3e9936f00ef7c01cf700e1
quotto <rhyme.and.rhyme@gmail.com>
2016-03-15 09:40:35 +0900

    fix deploy: passenger command

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,7 +57,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, "#{fetch :current_path}/tmp/restart.txt"
-      execute "passenger-config", "restart-app"
+      execute "passenger-config", "restart-app --name #{fetch :current_path}"
     end
   end
 
